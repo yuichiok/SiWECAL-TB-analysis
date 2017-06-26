88550bc8175343cc749f4f68612e18574b2428c2
Artur Lobanov <arturlbv@gmail.com>
2017-06-26 16:46:35 +0200

    further argument optimization

--- batch_convert.py
+++ batch_convert.py
@@ -4,15 +4,10 @@ import ROOT as rt
 rt.gROOT.LoadMacro("mergeRootFiles.C+")
 from build_events import *
 
-maxEntries = -1#300
-dif_pattern = "_dif_1_1_2.raw.root"
-force_merge = True
-force_build = True
-
 def convert_dir(indir,opts):
 
     ## search input dir for files matching run*_dif_1_1_1.raw.root
-    pattern = "run_*" + dif_pattern
+    pattern = "run_*" + opts.dif_suffix
 
     fnames = []
     if os.path.exists(indir):
@@ -21,13 +16,13 @@ def convert_dir(indir,opts):
                 fnames.append(root + "/" + items)
     else:
         # if input pattern already given
-        fnames = glob.glob(indir + dif_pattern)
+        fnames = glob.glob(indir + opts.dif_suffix)
 
     print("# Found %i runs" % len(fnames))
 
     for fname in fnames:
         # analyze run
-        run_name = os.path.basename(fname).replace(dif_pattern,"")
+        run_name = os.path.basename(fname).replace(opts.dif_suffix,"")
         run_dir =  os.path.dirname(fname)
 
         print 80*"#"
@@ -47,7 +42,7 @@ def convert_dir(indir,opts):
         build_fname = run_pattern + "_build.root"
         if not os.path.exists(build_fname) or opts.force_build:
             print("### Building events from merged file")
-            build_events(merge_fname,maxEntries)
+            build_events(merge_fname,opts.maxEntries)
         else:
             print("### Already built!")
 
@@ -56,10 +51,11 @@ def convert_dir(indir,opts):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Batch convert (merge&build) ecal runs.')
-    #parser.add_argument('-i','--indir', dest='indir', help='input run dir')
     parser.add_argument('indirs', nargs='+', help='input run dirs')
     parser.add_argument('--force-merge', dest='force_merge', action='store_true', help='Force merge files (default: False)')
     parser.add_argument('--force-build', dest='force_build', action='store_true', help='Force build files (default: False)')
+    parser.add_argument('-n','--maxEntries', dest='maxEntries', type=int, default = -1, help='maximum number of entries to process (default: all)')
+    parser.add_argument('--dif-pattern', dest='dif_suffix', default = "_dif_1_1_2.raw.root", help='single dif/slab suffix pattern')
 
     args = parser.parse_args()
 
