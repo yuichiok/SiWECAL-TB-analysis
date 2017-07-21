699386ca3c4ebeb4fb0ed5f400910a302e6981f3
irles <irles@lal.in2p3.fr>
2017-07-21 19:50:59 +0200

    scripts for signals analysis

--- singleslab/SigAnalysis.cc
+++ singleslab/SigAnalysis.cc
@@ -5,7 +5,7 @@
 void SigAnalysis(TString filename_in = "dif_1_1_1"){
   //, TString filename_out,TString filename_grid) {
 
-  filename_in="/home/irles/cernbox/TB2017/TBdata/MIPscan/rootfiles_bcidTh15/"+filename_in+".raw.root"
+  filename_in="/home/irles/cernbox/TB2017/TBdata/MIPscan/rootfiles_bcidTh15/"+filename_in+".raw.root";
   singleSlabAnalysis ss(filename_in); 
   ss.SignalAnalysis();
   
