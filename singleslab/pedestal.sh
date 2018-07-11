5ec90531bab4192591990b730219b50d26b8be77
airqui <irles@lal.in2p3.fr>
2018-07-11 12:23:55 +0200

    remove unused stuff

--- singleslab/pedestal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## For all grid points, merge all runs with same grid number and change naming to have two numbers (00-80)
-
-
-root -l PedAnalysis.cc\(\"/home/irles/WorkAreaECAL/2018/TB201807/data/longslab_20180703/DESY_longslab_beamon2_thr250_dif_1_1_1.raw.root\",\"dif_$dif\",\"beamon2_th250\"\) 
