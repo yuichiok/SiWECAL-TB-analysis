c4ffea7cae7a10f5dea4ba6784242f03520c1a3e
irles <irles@lal.in2p3.fr>
2019-08-02 17:35:51 +0200

    Simple example to analyze the event built files

---
+++ proto/run_example.cc
@@ -0,0 +1,14 @@
+#include "TROOT.h"
+#include "TFile.h"
+#include "example.cc"
+
+void run_example(){
+
+  TString run="run_32002";
+  TString filename = "/home/irles/WorkAreaECAL/2019/TB201906/EvBuilt/"+run+"_build.root";
+  example ss(filename);
+  ss.SimpleDistributionsTrack("_"+run);
+  ss.SimpleEvDisplayTrack("_"+run);
+  gSystem->Exit(0);
+
+}
