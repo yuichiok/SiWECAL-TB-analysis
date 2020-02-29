086aab23dfab28903ad294d1bb7028f164c512a1
calice <irles@lal.in2p3.fr>
2020-02-29 16:31:18 +0100

    clean DIF stuff

--- singleslab/performance/analysis.sh
+++
@@ -1,5 +0,0 @@
-run=$1
-slab=$2
-root -l -q analysis.cc\(\"$run\",$slab\) 
-
-
--- singleslab/runs21_slb.sh
+++
@@ -1,6 +0,0 @@
-#source analysis_2slb.sh 21001 0
-
-for i in {21001..21048}
-do
-    source analysis_2slb.sh $i 0
-done
