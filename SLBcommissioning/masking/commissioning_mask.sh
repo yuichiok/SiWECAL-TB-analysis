bc8b0326f144a201888f3899fc67fa51b4f7290e
airqui <irles@lal.in2p3.fr>
2020-06-04 11:08:17 +0200

    modifications on the script to find the right folders with software and data

--- SLBcommissioning/masking/commissioning_mask.sh
+++ SLBcommissioning/masking/commissioning_mask.sh
@@ -24,8 +24,11 @@ if [[ $3 == "" ]]
 fi
 
 
-data_softw=$PWD"/../../"
-mkdir /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}
+softw_path=$PWD"/../../"
+if [ ! -d "${softw_path}/SLBcommissioning/${date}" ]; then
+    mkdir ${soft_path}/SLBcommissioning/${date}  
+fi
+data_path="/mnt/win2/"
 
 
 if [[ $round == "first1" || $round == "first2" || $round == "first3" ]]
@@ -33,20 +36,20 @@ then
     for i in 0 1
     do
 	run="Run_ILC_"${date}"_"${round}"_"${i}"_Ascii"
-	mkdir ${data_softw}/converter_SLB/convertedfiles/${run}
-	output=${data_softw}"/converter_SLB/convertedfiles/"${run}"/"
+	mkdir ${softw_path}/converter_SLB/convertedfiles/${run}
+	output=${softw_path}"/converter_SLB/convertedfiles/"${run}"/"
 	
 	cd ../../converter_SLB
-	data_folder="/mnt/win2/Run_Data/"${run}"/"
+	data_folder=${data_path}"/Run_Data/"${run}"/"
 	root -l -q ConvertDirectorySL.cc\(\"${data_folder}\",false,\"${output}\"\) 
 	hadd ${output}/../Run_ILC_${date}_${round}_${i}.root ${output}/*.root 
 	cd -
     done
 
-    cp /mnt/win2/Run_Data/Run_ILC_${date}_${round}_0_Ascii/Run_Settings.txt  /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_it${it}.txt
+    cp ${data_path}/Run_Data/Run_ILC_${date}_${round}_0_Ascii/Run_Settings.txt  ${softw_path}/SLBcommissioning/${date}/Run_Settings_it${it}.txt
     root -l -q SimpleNoiseChecks.cc\(\"${date}\",\"${round}\",${it}\)
     it2=$((it+1))
-    cp /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt /mnt/win2/Setup/.
+    cp ${softw_path}/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt ${data_path}/Setup/.
 fi
 
 
@@ -54,29 +57,29 @@ if [[ $round == "second" ]]
 then
 
     run="Run_ILC_"${date}"_"${round}"_"${it}"_Ascii"
-    mkdir ${data_softw}"/converter_SLB/convertedfiles/"${run}
-    output=${data_softw}"/converter_SLB/convertedfiles/"${run}"/"
+    mkdir ${softw_path}"/converter_SLB/convertedfiles/"${run}
+    output=${softw_path}"/converter_SLB/convertedfiles/"${run}"/"
     
     cd ../../converter_SLB
-    data_folder="/mnt/win2/Run_Data/"${run}"/"
+    data_folder=${data_path}"/Run_Data/"${run}"/"
     root -l -q ConvertDirectorySL.cc\(\"${data_folder}\",false,\"${output}\"\)
     hadd ${output}/../Run_ILC_${date}_${round}_${it}.root ${output}/*.root
     cd -
     
-    cp /mnt/win2/Run_Data/Run_ILC_${date}_${round}_${it}_Ascii/Run_Settings.txt  /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_it${it}.txt
+    cp ${data_path}/Run_Data/Run_ILC_${date}_${round}_${it}_Ascii/Run_Settings.txt  ${softw_path}/SLBcommissioning/${date}/Run_Settings_it${it}.txt
     root -l -q SimpleNoiseChecks.cc\(\"${date}\",\"${round}\",${it}\)
     it2=$((it+1))
-    cp /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt /mnt/win2/Setup/.
+    cp ${softw_path}/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt ${data_path}/Setup/.
 fi
 
 if [[ $round == "scurves" ]]
 then
     echo "DID YOU SET UP CORRECTLY THE SLBOARD-ADDress mapping in scurves.C ???"
-    cp /mnt/win2/Histos/RateVsThresholdScan_${date}_SLBoard.txt ../${date}/.
-    cp /mnt/win2/Run_Data/Run_ILC_${date}_second_${it}_Ascii/Run_Settings.txt /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_it${it}.txt
+    cp ${data_path}/Histos/RateVsThresholdScan_${date}_SLBoard.txt ../${date}/.
+    cp ${data_path}/Run_Data/Run_ILC_${date}_second_${it}_Ascii/Run_Settings.txt ${softw_path}/SLBcommissioning/${date}/Run_Settings_it${it}.txt
     root -l -q scurves.C\(\"${date}\",${it}\)
     it2=$((it+1))
-    cp /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt /mnt/win2/Setup/.
+    cp ${softw_path}/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt ${data_path}/Setup/.
     cp histos/scurves_${date}.root ../${date}/.
 fi
 
@@ -87,17 +90,17 @@ if [[ $round == "cosmic" ]]
 then
 
     run="Run_ILC_"${date}"_"${round}"_"${it}"_Ascii"
-    mkdir ${data_softw}"/converter_SLB/convertedfiles/"${run}
-    output=${data_softw}"/converter_SLB/convertedfiles/"${run}"/"
+    mkdir ${softw_path}"/converter_SLB/convertedfiles/"${run}
+    output=${softw_path}"/converter_SLB/convertedfiles/"${run}"/"
 
     cd ../../converter_SLB
-    data_folder="/mnt/win2/Run_Data/"${run}"/"
+    data_folder=${data_path}"/Run_Data/"${run}"/"
     root -l -q ConvertDirectorySL.cc\(\"${data_folder}\",false,\"${output}\"\)
     hadd ${output}/../Run_ILC_${date}_${round}_${it}.root ${output}/*.root
     cd -
 
-    cp /mnt/win2/Run_Data/Run_ILC_${date}_${round}_${it}_Ascii/Run_Settings.txt  /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_it${it}.txt
+    cp ${data_path}/Run_Data/Run_ILC_${date}_${round}_${it}_Ascii/Run_Settings.txt  ${softw_path}/SLBcommissioning/${date}/Run_Settings_it${it}.txt
     root -l -q SimpleNoiseChecks.cc\(\"${date}\",\"${round}\",${it}\)
     it2=$((it+1))
-    cp /home/calice/TB2020/commissioning/SiWECAL-TB-analysis_test/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt /mnt/win2/Setup/.
+    cp ${softw_path}/SLBcommissioning/${date}/Run_Settings_comm_it${it2}.txt ${data_path}/Setup/.
 fi
