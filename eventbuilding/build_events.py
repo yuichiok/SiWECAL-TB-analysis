81f123ab37a4b2e9ff064bdb16e0209ca7096831
Jonas Kunath <jonas.kunath@gmx.de>
2021-10-11 15:25:30 +0200

    Eventbuilding: change build_events into a class

--- eventbuilding/build_events.py
+++ eventbuilding/build_events.py
@@ -10,6 +10,27 @@ from help_tools import *
 
 BCID_VALEVT = 50  # TODO: Put this into EcalNumbers?
 
+
+try:
+    from tqdm.autonotebook import tqdm
+
+    def get_tree_events(tree, max_entries):
+        for i, event in enumerate(tqdm(tree, desc="# Build events: ", total=max_entries)):
+            if i > max_entries:
+                break
+            yield i, event
+except ImportError:
+    def get_tree_events(tree, max_entries):
+        print("# Going to analyze %i entries..." %max_entries)
+        print("# For better progress information: `pip install tqdm`.")
+        for i, event in enumerate(tree):
+            if i > max_entries:
+                break
+            if i%100 == 0:
+                print("Entry %i" %i)
+            yield i, event
+
+
 def get_corr_bcid(bcid):
     #if bcid > 10: return bcid
     if bcid > BCID_VALEVT: return bcid
@@ -116,92 +137,141 @@ def get_hits(entry, bcid_map, ecal_config):
     return event
 
 
-def build_events(
-    file_name,
-    w_config=-1,
-    max_entries=-1,
-    out_file_name=None,
-    ecal_numbers=None,  # Not provided in CLI. Mainly useful for debugging/changing.
-):
-    ecal_config = EcalConfig(w_config=w_config, numbers=ecal_numbers)
+class BuildEvents:
+    _in_tree_name = "siwecaldecoded"
+    _out_tree_name = "ecal"
 
-    # Get ttree
-    tfile = rt.TFile(file_name,"read")
-    tree_name = "siwecaldecoded"
-    tree = tfile.Get(tree_name)
-    if not tree:
-        print("Found tree names:", [k.GetName() for k in tfile.GetListOfKeys()])
-        raise EventBuildingException("Tree %s not found in %s" %(tree_name, file_name))
+    _branch_tags = {
+        "event info": [
+            "event/I",
+            "spill/I",
+            "bcid/I",
+            "prev_bcid/I",
+            "next_bcid/I",
+        ],
+        "hit summary": [
+            "nhit_slab/I",
+            "nhit_chip/I",
+            "nhit_chan/I",
+            "sum_hg/F",
+            "sum_energy/F",
+        ],
+        "hit id": [
+            "hit_slab[nhit_chan]/I",
+            "hit_chip[nhit_chan]/I",
+            "hit_chan[nhit_chan]/I",
+            "hit_sca[nhit_chan]/I",
+        ],
+        "hit coord": [
+            "hit_x[nhit_chan]/F",
+            "hit_y[nhit_chan]/F",
+            "hit_z[nhit_chan]/F",
+            "hit_x0[nhit_chan]/F",
+        ],
+        "hit readout": [
+            "hit_hg[nhit_chan]/F",
+            "hit_lg[nhit_chan]/F",
+            "hit_energy[nhit_chan]/F",
+        ],
+        "hit booleans": [
+            "hit_isHit[nhit_chan]/I",
+            "hit_isMasked[nhit_chan]/I",
+        ],
+    }
 
-    ##### TREE #####
-    if out_file_name is None:
-        if file_name.endswith("_converted.root"):
-            out_file_name = file_name[:-len("_converted.root")] + "_build.root"
-        elif file_name.endswith(".root"):
-            out_file_name = file_name[:-len(".root")] + "_build.root"
+    def __init__(
+        self,
+        file_name,
+        w_config=-1,
+        max_entries=-1,
+        out_file_name=None,
+        ecal_numbers=None,  # Not provided in CLI. Mainly useful for debugging/changing.
+    ):
+        self.ecal_config = EcalConfig(w_config=w_config, numbers=ecal_numbers)
+        self.file_name = file_name
+        self.max_entries = max_entries
+        self.out_file_name = out_file_name
+
+
+    def _get_tree(self, file_name):
+        self.in_file = rt.TFile(file_name,"read")
+        self.tree = self.in_file.Get(self._in_tree_name)
+        if not self.tree:
+            trees_available = [k.GetName() for k in self.in_file.GetListOfKeys()]
+            print("Found tree names:", trees_available)
+            ex_txt = "Tree %s not found in %s" %(self._in_tree_name, file_name)
+            raise EventBuildingException(ex_txt)
+        return self.tree
+
+
+    def _add_branch(self, tag):
+        name, branch_type = tag.split("/")
+        branch_type = {"I": "i", "F": "f"}[branch_type]
+        array_indicator = "[nhit_chan]"
+        if array_indicator in name:
+            name = name.replace(array_indicator, "")
+            starting_value = 10000 * [0]
         else:
-            raise EventBuildingException("Unexpected file extension: %s" %file_name)
-    outf = rt.TFile(out_file_name,"recreate")
-    outtree = rt.TTree("ecal","Build ecal events")
+            starting_value = [0]
+        self.out_arrays[name] = array(branch_type, starting_value)
+        self.out_tree.Branch(name, self.out_arrays[name], tag)
 
-    print("# Creating ecal tree in file %s" %out_file_name)
 
-    #### BRANCHES
-    # event info
-    event = array('i', [0]); outtree.Branch( 'event', event, 'event/I' )
-    spill = array('i', [0]); outtree.Branch( 'spill', spill, 'spill/I' )
-    bcid_b = array('i', [0]); outtree.Branch( 'bcid', bcid_b, 'bcid/I' )
-    prev_bcid_b = array('i', [0]); outtree.Branch( 'prev_bcid', prev_bcid_b, 'prev_bcid/I' )
-    next_bcid_b = array('i', [0]); outtree.Branch( 'next_bcid', next_bcid_b, 'next_bcid/I' )
+    def _create_out_tree(self, out_file_name, file_name):
+        if out_file_name is None:
+            if file_name.endswith("_converted.root"):
+                out_file_name = file_name[:-len("_converted.root")] + "_build.root"
+            elif file_name.endswith(".root"):
+                out_file_name = file_name[:-len(".root")] + "_build.root"
+            else:
+                raise EventBuildingException("Unexpected file extension: %s" %file_name)
+        print("# Creating ecal tree in file %s" %out_file_name)
+        self.out_file = rt.TFile(out_file_name,"recreate")
+        self.out_tree = rt.TTree(self._out_tree_name, "Build ecal events")
 
-    # occupancy/hit info
-    nhit_slab = array('i', [0]); outtree.Branch( 'nhit_slab', nhit_slab, 'nhit_slab/I' )
-    nhit_chip = array('i', [0]); outtree.Branch( 'nhit_chip', nhit_chip, 'nhit_chip/I' )
-    nhit_chan = array('i', [0]); outtree.Branch( 'nhit_chan', nhit_chan, 'nhit_chan/I' )
-    sum_hg = array('f', [0]); outtree.Branch( 'sum_hg', sum_hg, 'sum_hg/F' )
-    sum_energy = array('f', [0]); outtree.Branch( 'sum_energy', sum_energy, 'sum_energy/F' )
+        for branch_tag in [bt for bts in self._branch_tags.values() for bt in bts]:
+            self._add_branch(branch_tag)
+        return self.out_tree
 
-    ## hit information
-    # detid
-    hit_slab = array('i', 10000*[0]); outtree.Branch( 'hit_slab', hit_slab, 'hit_slab[nhit_chan]/I' )
-    hit_chip = array('i', 10000*[0]); outtree.Branch( 'hit_chip', hit_chip, 'hit_chip[nhit_chan]/I' )
-    hit_chan = array('i', 10000*[0]); outtree.Branch( 'hit_chan', hit_chan, 'hit_chan[nhit_chan]/I' )
-    hit_sca = array('i', 10000*[0]); outtree.Branch( 'hit_sca', hit_sca, 'hit_sca[nhit_chan]/I' )
-    # coord
-    hit_x = array('f', 10000*[0]); outtree.Branch( 'hit_x', hit_x, 'hit_x[nhit_chan]/F' )
-    hit_y = array('f', 10000*[0]); outtree.Branch( 'hit_y', hit_y, 'hit_y[nhit_chan]/F' )
-    hit_z = array('f', 10000*[0]); outtree.Branch( 'hit_z', hit_z, 'hit_z[nhit_chan]/F' )
-    hit_x0 = array('f', 10000*[0]); outtree.Branch( 'hit_x0', hit_x0, 'hit_x0[nhit_chan]/F' )
-    # energy
-    hit_hg = array('f', 10000*[0]); outtree.Branch( 'hit_hg', hit_hg, 'hit_hg[nhit_chan]/F' )
-    hit_lg = array('f', 10000*[0]); outtree.Branch( 'hit_lg', hit_lg, 'hit_lg[nhit_chan]/F' )
-    hit_energy = array('f', 10000*[0]); outtree.Branch( 'hit_energy', hit_energy, 'hit_energy[nhit_chan]/F' )
-    # boolean
-    hit_isHit = array('i', 10000*[0]); outtree.Branch( 'hit_isHit', hit_isHit, 'hit_isHit[nhit_chan]/I' )
-    hit_isMasked = array('i', 10000*[0]); outtree.Branch( 'hit_isMasked', hit_isMasked, 'hit_isMasked[nhit_chan]/I' )
 
-    if max_entries < 0: max_entries = tree.GetEntries()
-    #else: max_entries = 1000
+    def _write_and_close(self):
+        self.out_tree.Write()
+        # self.out_tree.Print()
+        print("# Created tree with %i events." % self.out_tree.GetEntries())
+        self.out_file.Close()
+        self.in_file.Close()
 
-    spill_cnt = 0
 
-    print("# Going to analyze %i entries..." %max_entries )
-    for ientry,entry in enumerate(tree):#.GetEntries():
+    def build_events(self, file_name=None, max_entries=None, out_file_name=None):
+        if file_name is None:
+            file_name = self.file_name
+        if out_file_name is None:
+            out_file_name = self.out_file_name
+        self.in_tree = self._get_tree(file_name)
+        self.out_arrays = {}
+        self.out_tree = self._create_out_tree(out_file_name, file_name)
 
-        if ientry > max_entries: break
-        #if ientry != 9: continue
+        if max_entries is None:
+            max_entries = self.max_entries
+        if max_entries < 0:
+            max_entries = self.in_tree.GetEntries()
 
-        if ientry%100 == 0: print("Entry %i" %ientry)
+        print(max_entries)
+        for i_spill, entry in get_tree_events(self.in_tree, max_entries):
+            self._fill_spill(i_spill, entry)
+        self._write_and_close()
+
+
+    def _fill_spill(self, spill, entry):
+        b = self.out_arrays
+        b["spill"][0] = spill
 
         ## BCID
         bcids = get_good_bcids(entry)
         bcid_map = merge_bcids(bcids)
 
-        spill[0] = spill_cnt
-        spill_cnt += 1
-
         ## Collect hits in bcid container
-        ev_hits = get_hits(entry, bcid_map, ecal_config)
+        ev_hits = get_hits(entry, bcid_map, self.ecal_config)
 
         #for bcid,hits in ev_hits.items():
         for ibc,bcid in enumerate(sorted(ev_hits)):
@@ -214,52 +284,45 @@ def build_events(
             corr_bcid = get_corr_bcid(bcid)
             global event_counter
             event_counter = event_counter+1
-            event[0] = event_counter#int(spill[0]*10000 + corr_bcid)
-            bcid_b[0] = corr_bcid
+            b["event"][0] = event_counter#int(spill[0]*10000 + corr_bcid)
+            b["bcid"][0] = corr_bcid
 
             ## store distance to previous bcid
             if ibc > 0:
                 prev_bcid = sorted(ev_hits)[ibc -1]
-                prev_bcid_b[0] = get_corr_bcid(prev_bcid)
+                b["prev_bcid"][0] = get_corr_bcid(prev_bcid)
             else:
-                prev_bcid_b[0] = -1
+                b["prev_bcid"][0] = -1
 
             if ibc + 1 < len(ev_hits):
                 next_bcid = sorted(ev_hits)[ibc +1]
-                next_bcid_b[0] = get_corr_bcid(next_bcid)
-                #print("ibc=%i length=%i bcid=%i spill=%i"%(ibc,len(ev_hits),next_bcid_b[0],spill[0]))
+                b["next_bcid"][0] = get_corr_bcid(next_bcid)
+                #print("ibc=%i length=%i bcid=%i spill=%i"%(ibc,len(ev_hits),b["next_bcid"][0],spill[0]))
             else:
-                next_bcid_b[0] = -1
+                b["next_bcid"][0] = -1
 
             # count hits per slab/chan/chip
-            nhit_slab[0] = len(set([hit.slab for hit in hits]))
-            nhit_chip[0] = len(set([(hit.slab*ecal_config._N.n_chips + hit.chip) for hit in hits]))
-            #nhit_chan[0] = len(set([(hit.slab*ecal_config._N.n_chips + hit.chip)*ecal_config._N.n_channels + hit.chan for hit in hits]))
-            nhit_chan[0] = len(hits)
-            sum_hg[0] = sum([hit.hg for hit in hits])
-            sum_energy[0] = sum([hit.energy for hit in hits])
+            b["nhit_slab"][0] = len(set([hit.slab for hit in hits]))
+            b["nhit_chip"][0] = len(set([(hit.slab*self.ecal_config._N.n_chips + hit.chip) for hit in hits]))
+            # b["nhit_chan"][0] = len(set([(hit.slab*self.ecal_config._N.n_chips + hit.chip)*self.ecal_config._N.n_channels + hit.chan for hit in hits]))
+            b["nhit_chan"][0] = len(hits)
+            b["sum_hg"][0] = sum([hit.hg for hit in hits])
+            b["sum_energy"][0] = sum([hit.energy for hit in hits])
 
             if len(hits) > 8000:
-                print("Suspicious number of hits! %i for bcid %i and previous bcid %i" %(len(hits),bcid_b[0],prev_bcid_b[0]))
-                print("Skipping event %i" % event[0] )
+                print("Suspicious number of hits! %i for bcid %i and previous bcid %i" %(len(hits),b["bcid"][0],b["prev_bcid"][0]))
+                print("Skipping event %i" % b["event"][0] )
                 continue
 
             for i,hit in enumerate(hits):
-                hit_slab[i] = hit.slab; hit_chip[i] = hit.chip; hit_chan[i] = hit.chan; hit_sca[i] = hit.sca
-                hit_x[i] = hit.x; hit_y[i] = hit.y; hit_z[i] = hit.z; hit_x0[i] = hit.x0
-                hit_hg[i] = hit.hg; hit_lg[i] = hit.lg
-                hit_isHit[i] = hit.isHit; hit_isMasked[i] = hit.isMasked
+                b["hit_slab"][i] = hit.slab; b["hit_chip"][i] = hit.chip; b["hit_chan"][i] = hit.chan; b["hit_sca"][i] = hit.sca
+                b["hit_x"][i] = hit.x; b["hit_y"][i] = hit.y; b["hit_z"][i] = hit.z; b["hit_x0"][i] = hit.x0
+                b["hit_hg"][i] = hit.hg; b["hit_lg"][i] = hit.lg
+                b["hit_isHit"][i] = hit.isHit; b["hit_isMasked"][i] = hit.isMasked
 
-                hit_energy[i] = hit.energy
+                b["hit_energy"][i] = hit.energy
 
-            outtree.Fill()
-
-    outtree.Write()
-    #outtree.Print()
-    print("# Created tree with %i events" % outtree.GetEntries())
-    outf.Close()
-
-    tfile.Close()
+            self.out_tree.Fill()
 
 
 if __name__ == "__main__":
@@ -270,4 +333,4 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--max_entries", default=-1, type=int)
     parser.add_argument("-w", "--w_config", default=-1, type=int)
     parser.add_argument("-o", "--out_file_name", default=None)
-    build_events(**vars(parser.parse_args()))
+    BuildEvents(**vars(parser.parse_args())).build_events()
