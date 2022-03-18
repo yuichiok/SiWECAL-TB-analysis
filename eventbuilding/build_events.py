bd670e26b5fd8c79e4af70bc5e38fc0aa6df64ec
Jonas Kunath <jonas.kunath@gmx.de>
2022-03-18 09:47:14 +0100

    Event building: Improve the BCID overflow correction (issue #29)
    Longer description on why this is necessary (and if it does the
    right thing now) in the corresponding GitHub issue:
    https://github.com/SiWECAL-TestBeam/SiWECAL-TB-analysis/issues/29

--- eventbuilding/build_events.py
+++ eventbuilding/build_events.py
@@ -56,13 +56,19 @@ class BCIDHandler:
         self._min_slabs_hit = min_slabs_hit
 
 
-    def _apply_overflow_correction(self, arr):
+    def _calculate_overflow_correction(self, arr):
         """"Within a chips memory, the BCID must increase. A drop indicates overflow."""
         # assert np.max(arr) < self._N.bcid_overflow
         n_memory_cycles = np.cumsum(arr[:,1:] - arr[:,:-1] < 0, axis=1)
         is_filled = arr[:,1:] != self._N.bcid_bad_value
-        arr[:,1:] = arr[:,1:] + self._N.bcid_overflow * n_memory_cycles * is_filled
-        return arr
+        overflown_bcids = np.copy(arr)
+        overflown_bcids[:,1:] = arr[:,1:] + self._N.bcid_overflow * n_memory_cycles * is_filled
+        bcid_to_overflow_adjusted = {}
+        unique_overflow = np.unique(overflown_bcids)
+        unique_overflow = unique_overflow[unique_overflow >= self._N.bcid_overflow]
+        for of_bcid in np.unique(overflown_bcids):
+            bcid_to_overflow_adjusted[of_bcid % self._N.bcid_overflow] = of_bcid
+        return overflown_bcids, bcid_to_overflow_adjusted
 
 
     def _is_retrigger(self, arr):
@@ -87,7 +93,6 @@ class BCIDHandler:
             np.any(raw_bcids != self._N.bcid_bad_value, axis=1)
         )[0]
         bcids = np.copy(raw_bcids[chip_ids])
-        bcids = self._apply_overflow_correction(bcids)
         bcids[:,1:][self._is_retrigger(bcids)] = self._N.bcid_bad_value
         bcids[bcids < self._N.bcid_skip_noisy_acquisition_start] = self._N.bcid_bad_value
         for drop_bcid in self._N.bcid_drop:
@@ -129,40 +134,51 @@ class BCIDHandler:
             slabs_hit = pos // (self._N.n_chips * self._N.n_scas)
             if len(np.unique(slabs_hit)) >= self._min_slabs_hit:
                 pos_per_bcid[k] = pos
-        return pos_per_bcid, bcid_before_merge
+        return pos_per_bcid, bcid_before_merge, bcid_start_stop
 
 
     def load_spill(self, spill_entry):
         """False if the current spill is empty."""
         chip_ids, bcids = self._get_bcids(spill_entry.bcid)
-        self.bcid_first_sca_full = self._find_bcid_filling_last_sca(bcids)
-
-        self.pos_per_bcid, self._bcid_before_merge = \
+        overflown_bcids, self.bcid_to_overflow_adjusted = \
+            self._calculate_overflow_correction(bcids)
+        self.bcid_first_sca_full = self._find_bcid_filling_last_sca(overflown_bcids)
+        self.pos_per_bcid, self._bcid_before_merge, bcid_start_stop = \
             self._get_array_positions_per_bcid(chip_ids, bcids)
-        self.spill_bcids = sorted(self.pos_per_bcid.keys())
+        self.spill_bcids_overflown = []
+        for start_bcid in self.pos_per_bcid.keys():
+            n_memory_cycles = max([
+                self.bcid_to_overflow_adjusted.get(b, b)
+                for b in range(start_bcid, bcid_start_stop[start_bcid] + 1)
+            ]) // self._N.bcid_overflow
+            _overflown = start_bcid + n_memory_cycles * self._N.bcid_overflow
+            self.spill_bcids_overflown.append(_overflown)
+        self.spill_bcids_overflown = sorted(self.spill_bcids_overflown)
         return len(self.pos_per_bcid) > 0
 
 
     def yield_from_spill(self):
-        for bcid in self.spill_bcids:
+        for overflown_bcid in self.spill_bcids_overflown:
+            bcid = overflown_bcid % self._N.bcid_overflow
             before_merge = self._bcid_before_merge[bcid]
+            plus_overflow = (overflown_bcid // self._N.bcid_overflow) * self._N.bcid_overflow
             for i, pos in enumerate(self.pos_per_bcid[bcid]):
-                yield pos, bcid, before_merge[i]
+                yield pos, bcid + plus_overflow, before_merge[i] + plus_overflow
 
 
-    def previous_bcid(self, bcid):
-        i_bcid = self.spill_bcids.index(bcid)
+    def previous_bcid(self, overflown_bcid):
+        i_bcid = self.spill_bcids_overflown.index(overflown_bcid)
         if i_bcid == 0:
             return -1
         else:
-            return self.spill_bcids[i_bcid - 1]
+            return self.spill_bcids_overflown[i_bcid - 1]
 
-    def next_bcid(self, bcid):
-        i_bcid = self.spill_bcids.index(bcid)
-        if i_bcid == len(self.spill_bcids) - 1:
+    def next_bcid(self, overflown_bcid):
+        i_bcid = self.spill_bcids_overflown.index(overflown_bcid)
+        if i_bcid == len(self.spill_bcids_overflown) - 1:
             return -1
         else:
-            return self.spill_bcids[i_bcid + 1]
+            return self.spill_bcids_overflown[i_bcid + 1]
 
 
 def get_hits_per_event(entry, bcid_handler, ecal_config):
