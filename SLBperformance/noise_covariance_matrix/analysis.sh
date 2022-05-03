7d0efc1172d9866a837ccbfcd56e0795d7a9856b
yuichiok <yuichi.okugawa@cern.ch>
2022-05-03 14:58:49 +0200

    fix path for analysis

--- SLBperformance/noise_covariance_matrix/analysis.sh
+++ SLBperformance/noise_covariance_matrix/analysis.sh
@@ -1,11 +1,11 @@
 run=$1
-output="/eos/project-s/siw-ecal/TB2021-11/beamData/rootfiles/3GeVMIPscan/"
+output="/eos/project/s/siw-ecal/TB2021-11/beamData/rootfiles/3GeVMIPscan/"
 
 for sca in {0..14}
 do
     cd ../
-    root -l -q Noise.cc\(\"${output}/${run}.root\",\"${run}\",0,${sca}\)
-    root -l -q Noise.cc\(\"${output}/${run}.root\",\"${run}\",1,${sca}\)
+    root -l -q Noise.cc\(\"${output}/${run}_merged.root\",\"${run}\",0,${sca}\)
+    root -l -q Noise.cc\(\"${output}/${run}_merged.root\",\"${run}\",1,${sca}\)
     cd -
 done
 
