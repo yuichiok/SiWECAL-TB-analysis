a691470cdc326bf4f65e700ff578f7cf78911402
yuichiok <yuichi.okugawa@cern.ch>
2023-12-12 13:09:56 +0900

    Add fitMIPs.C macro for MIP calibration

--- macros/drawMIPs.C
+++ macros/drawMIPs.C
@@ -12,122 +12,80 @@
 #include <fstream>
 #include "TF1.h"
 #include "Styles.hh"
-#include "../SLBperformance/TBchecks/analysis.h"
-
-// #include "../SLBco/conf_struct.h"
-
-// #include "../../style/Style.C"
-// #include "../../style/Labels.C"
+#include "../SLBcommissioning/conf_struct.h"
 
 using namespace std;
 
 int nslabs = 15;
-// int nchips = 16;
+int nchips = 16;
 int nchannels = 64;
-int nchannels_fit = 6;
 
-Float_t charge_low[16][64];  // daughter, slab, asic, chn, DAC
-Float_t charge_high[16][64]; // daughter, slab, asic, chn, DAC
+int iSCA = 0;
+
+TH2F* h2XY(TH2F* h2, int slab)
+{
+  TString hname = h2->GetName();
+  TH2F *map_XY = new TH2F(hname+"_XY",hname+"_XY", 32, -90, 90, 32, -90, 90);
+
+  TString map_name = "../mapping/fev10_chip_channel_x_y_mapping.txt";
+  ReadMap(map_name);
+
+
+
+  int nbinsX = h2->GetNbinsX();
+  int nbinsY = h2->GetNbinsY();
+
+  for( int ichip=0; ichip<nchips; ichip++ ){
+    // cout << "=== chip " << ichip << " ===" << endl;
+    for( int ichn=0; ichn<nchannels; ichn++ ){
+
+      int val_x = slab*20  + ichip;
+      int val_y = iSCA*100 + ichn;
+
+      int bix_x = h2->GetXaxis()->FindBin(val_x);
+      int bix_y = h2->GetYaxis()->FindBin(val_y);
+
+      int bin = h2->GetBin(bix_x, bix_y);
+
+      float mip = h2->GetBinContent(bin);
+
+      float x = map_pointX[ichip][ichn];
+      float y = map_pointY[ichip][ichn];
+
+      // cout << x << " " << y << " " << ped << endl;
+      map_XY->Fill(x, y, mip);
+
+    }
+  }
+
+  map_XY->SetTitle(";x [mm];y [mm]");
+  return map_XY;
+
+}
 
 void drawMIPs()
 {
-  TFile *file_MIPSummary = new TFile("../mip_calib/MIPSummary_pedestalsubmode1_raw_siwecal_90021to90070_highgain.root", "READ");
+  gStyle->SetOptStat(0);
 
-  TCanvas * mip_all = new TCanvas("mip_all","mip_all",800,800);
-  mip_all->SetLeftMargin(0.12);
-  mip_all->SetRightMargin(0.15);
-  TH2F * h_mip_all = (TH2F*) file_MIPSummary->Get("mpv_all");
-  h_mip_all->SetTitle("");
-  h_mip_all->GetXaxis()->SetTitle("Layer #times 20 + Chip");
-  h_mip_all->GetYaxis()->SetTitle("Channel");
-  h_mip_all->GetZaxis()->SetTitle("MIP value [ADC]");
-  h_mip_all->GetZaxis()->SetTitleOffset(1.2);
-  h_mip_all->GetZaxis()->SetRangeUser(0, 50);
-  h_mip_all->Draw("colz");
+  TFile *file_MIPSummary = new TFile("../mip_calib/MIPSummary_pedestalsubmode1_raw_siwecal_90259to90285_highgain.root", "READ");
 
-  TCanvas * width_all = new TCanvas("width_all","width_all",800,800);
-  width_all->SetLeftMargin(0.12);
-  width_all->SetRightMargin(0.15);
-  TH2F * h_width_all = (TH2F*) file_MIPSummary->Get("width_all");
-  h_width_all->SetTitle("");
-  h_width_all->GetXaxis()->SetTitle("Layer #times 20 + Chip");
-  h_width_all->GetYaxis()->SetTitle("Channel");
-  h_width_all->GetZaxis()->SetTitle("MIP width [ADC]");
-  h_width_all->GetZaxis()->SetTitleOffset(1.2);
-  h_width_all->GetZaxis()->SetRangeUser(0, 10);
-  h_width_all->Draw("colz");
+  TH2F *h2_mip   = (TH2F*)file_MIPSummary->Get("mpv_all");
+  TH2F *h2_width = (TH2F*)file_MIPSummary->Get("width_all");
 
+  TH2F* mipXY   = h2XY(h2_mip, 7);
+  TH2F* widthXY = h2XY(h2_width, 7);
 
+  TCanvas *c_mip_XY = new TCanvas("c_mip_XY", "c_mip_XY", 800, 800);
+  TPad *p_mip_XY    = new TPad("p_mip_XY", "p_mip_XY", 0,0,1,1);
+  StylePad(p_mip_XY,0.1,0.15,0.15,0.15);
+  mipXY->GetZaxis()->SetRangeUser(180, 300);
+  mipXY->Draw("colz");
 
-  Bool_t drawIt = true;
-  TCanvas *c_MIPSummary_layer7_0 = new TCanvas(TString::Format("MIPSummary_layer%i_0", 7), TString::Format("MIPSummary_layer%i_0", 7), 800, 800);
-  gStyle->SetOptStat(1111);
-  gStyle->SetOptFit(111);
-
-  for (int i = 0; i < nslabs; i++)
-  {
-    TCanvas * c_MIPSummary = (TCanvas*) file_MIPSummary->Get(TString::Format("MIPSummary_layer%i", i));
-    TList *primitives = c_MIPSummary->GetListOfPrimitives();
-    TIter next(primitives);
-    TObject* obj;
-    while ((obj = next())) {
-        TPad *pad = (TPad*)obj;
-        // StylePad(pad,0,0.15,0,0.17);
-        pad->SetLeftMargin(0.15);
-        pad->SetGrid(1,1);
-
-        TList *primitives2 = ((TPad*)obj)->GetListOfPrimitives();
-        TIter next2(primitives2);
-        TObject* obj2;
-        while ((obj2 = next2())) {
-          if (obj2->InheritsFrom("TH1")) {
-              TH1F* h = (TH1F*)obj2;
-              h->GetXaxis()->SetTitle("Charge [ADC]");
-              h->GetYaxis()->SetTitle("Entries");
-
-              if(i==7 && drawIt){
-                c_MIPSummary_layer7_0->cd();
-                c_MIPSummary_layer7_0->DrawFrame(0,0,1,1);
-                TH1F* h_0 = (TH1F*)h->Clone();
-                c_MIPSummary_layer7_0->SetGrid();
-                c_MIPSummary_layer7_0->SetLeftMargin(0.15);
-                std::vector<double> result = resultfit(h_0,"high");
-                double MIP     = result.at(0);
-                double MIP_err = result.at(1);
-                double MIP_w   = result.at(2);
-                double chi2ndf = result.at(3);
-                double MIP_w_err = result.at(4);
-
-                h_0->GetXaxis()->SetRangeUser(0, 100);
-                h_0->Draw();
-
-                TLatex latex;
-                latex.SetTextSize(0.025);
-                latex.SetTextAlign(13);  //align at top
-                // latex.DrawLatex(65,5000,TString::Format("MIP = %.2f #pm %.2f\\\\
-                // MIP width = %.2f #pm %.2f\\\\
-                // #chi^{2}/ndf = %.2f",
-                // MIP, MIP_err, MIP_w, MIP_w_err, chi2ndf));
-
-                latex.DrawLatex(60,4700,TString::Format("MIP = %.2f #pm %.2f", MIP, MIP_err));
-                latex.DrawLatex(60,4500,TString::Format("MIP width = %.2f #pm %.2f", MIP_w, MIP_w_err));
-                latex.DrawLatex(60,4300,TString::Format("#chi^{2}/ndf = %.2f", chi2ndf));
-                c_MIPSummary_layer7_0->Draw();
-
-
-                drawIt = false;
-              }
-
-              // h->Draw();
-          }
-        }
-
-    }
-
-    c_MIPSummary->cd();
-    // c_MIPSummary->Draw();
-    // c_MIPSummary->SaveAs(TString::Format("plots/MIPs/MIPSummary_layer%i.pdf", i));
-  }
+  TCanvas *c_width_XY = new TCanvas("c_width_XY", "c_width_XY", 800, 800);
+  TPad *p_width_XY    = new TPad("p_width_XY", "p_width_XY", 0,0,1,1);
+  StylePad(p_width_XY,0.1,0.15,0.15,0.15);
+  widthXY->GetZaxis()->SetRangeUser(1, 2);
+  widthXY->Draw("colz");
 
 }
 
---
+++ macros/fitMIPs.C
@@ -0,0 +1,133 @@
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TPaveStats.h"
+#include "TH1.h"
+#include "TROOT.h"
+#include "TRint.h"
+#include "TStyle.h"
+#include "TLegend.h"
+#include "TString.h"
+#include "TMath.h"
+#include <iostream>
+#include <fstream>
+#include "TF1.h"
+#include "Styles.hh"
+#include "../SLBperformance/TBchecks/analysis.h"
+
+// #include "../SLBco/conf_struct.h"
+
+// #include "../../style/Style.C"
+// #include "../../style/Labels.C"
+
+using namespace std;
+
+int nslabs = 15;
+// int nchips = 16;
+int nchannels = 64;
+int nchannels_fit = 6;
+
+Float_t charge_low[16][64];  // daughter, slab, asic, chn, DAC
+Float_t charge_high[16][64]; // daughter, slab, asic, chn, DAC
+
+void fitMIPs()
+{
+  TFile *file_MIPSummary = new TFile("../mip_calib/MIPSummary_pedestalsubmode1_raw_siwecal_90021to90070_highgain.root", "READ");
+
+  TCanvas * mip_all = new TCanvas("mip_all","mip_all",800,800);
+  mip_all->SetLeftMargin(0.12);
+  mip_all->SetRightMargin(0.15);
+  TH2F * h_mip_all = (TH2F*) file_MIPSummary->Get("mpv_all");
+  h_mip_all->SetTitle("");
+  h_mip_all->GetXaxis()->SetTitle("Layer #times 20 + Chip");
+  h_mip_all->GetYaxis()->SetTitle("Channel");
+  h_mip_all->GetZaxis()->SetTitle("MIP value [ADC]");
+  h_mip_all->GetZaxis()->SetTitleOffset(1.2);
+  h_mip_all->GetZaxis()->SetRangeUser(0, 50);
+  h_mip_all->Draw("colz");
+
+  TCanvas * width_all = new TCanvas("width_all","width_all",800,800);
+  width_all->SetLeftMargin(0.12);
+  width_all->SetRightMargin(0.15);
+  TH2F * h_width_all = (TH2F*) file_MIPSummary->Get("width_all");
+  h_width_all->SetTitle("");
+  h_width_all->GetXaxis()->SetTitle("Layer #times 20 + Chip");
+  h_width_all->GetYaxis()->SetTitle("Channel");
+  h_width_all->GetZaxis()->SetTitle("MIP width [ADC]");
+  h_width_all->GetZaxis()->SetTitleOffset(1.2);
+  h_width_all->GetZaxis()->SetRangeUser(0, 10);
+  h_width_all->Draw("colz");
+
+
+
+  Bool_t drawIt = true;
+  TCanvas *c_MIPSummary_layer7_0 = new TCanvas(TString::Format("MIPSummary_layer%i_0", 7), TString::Format("MIPSummary_layer%i_0", 7), 800, 800);
+  gStyle->SetOptStat(1111);
+  gStyle->SetOptFit(111);
+
+  for (int i = 0; i < nslabs; i++)
+  {
+    TCanvas * c_MIPSummary = (TCanvas*) file_MIPSummary->Get(TString::Format("MIPSummary_layer%i", i));
+    TList *primitives = c_MIPSummary->GetListOfPrimitives();
+    TIter next(primitives);
+    TObject* obj;
+    while ((obj = next())) {
+        TPad *pad = (TPad*)obj;
+        // StylePad(pad,0,0.15,0,0.17);
+        pad->SetLeftMargin(0.15);
+        pad->SetGrid(1,1);
+
+        TList *primitives2 = ((TPad*)obj)->GetListOfPrimitives();
+        TIter next2(primitives2);
+        TObject* obj2;
+        while ((obj2 = next2())) {
+          if (obj2->InheritsFrom("TH1")) {
+              TH1F* h = (TH1F*)obj2;
+              h->GetXaxis()->SetTitle("Charge [ADC]");
+              h->GetYaxis()->SetTitle("Entries");
+
+              if(i==7 && drawIt){
+                c_MIPSummary_layer7_0->cd();
+                c_MIPSummary_layer7_0->DrawFrame(0,0,1,1);
+                TH1F* h_0 = (TH1F*)h->Clone();
+                c_MIPSummary_layer7_0->SetGrid();
+                c_MIPSummary_layer7_0->SetLeftMargin(0.15);
+                std::vector<double> result = resultfit(h_0,"high");
+                double MIP     = result.at(0);
+                double MIP_err = result.at(1);
+                double MIP_w   = result.at(2);
+                double chi2ndf = result.at(3);
+                double MIP_w_err = result.at(4);
+
+                h_0->GetXaxis()->SetRangeUser(0, 100);
+                h_0->Draw();
+
+                TLatex latex;
+                latex.SetTextSize(0.025);
+                latex.SetTextAlign(13);  //align at top
+                // latex.DrawLatex(65,5000,TString::Format("MIP = %.2f #pm %.2f\\\\
+                // MIP width = %.2f #pm %.2f\\\\
+                // #chi^{2}/ndf = %.2f",
+                // MIP, MIP_err, MIP_w, MIP_w_err, chi2ndf));
+
+                latex.DrawLatex(60,4700,TString::Format("MIP = %.2f #pm %.2f", MIP, MIP_err));
+                latex.DrawLatex(60,4500,TString::Format("MIP width = %.2f #pm %.2f", MIP_w, MIP_w_err));
+                latex.DrawLatex(60,4300,TString::Format("#chi^{2}/ndf = %.2f", chi2ndf));
+                c_MIPSummary_layer7_0->Draw();
+
+
+                drawIt = false;
+              }
+
+              // h->Draw();
+          }
+        }
+
+    }
+
+    c_MIPSummary->cd();
+    // c_MIPSummary->Draw();
+    // c_MIPSummary->SaveAs(TString::Format("plots/MIPs/MIPSummary_layer%i.pdf", i));
+  }
+
+}
+
